fdd679e30c9deda0f76843fb016e0154c1625e46
Elodie0207 <elodieguldas@gmail.com>
2023-03-02 23:09:58 +0100

    Image Grenade

--- Map.py
+++ Map.py
@@ -5,7 +5,7 @@ class Map(pygame.sprite.Sprite):
     def __init__(self,x,y,width,height):
         super().__init__()
         self.image=pygame.Surface((width,height))
-        self.image.fill(White)
+        self.image=pygame.image.load("Image/grenade.png")
         self.rect=self.image.get_rect()
         self.rect.x=x
         self.rect.y=y
